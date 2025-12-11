e63304c7a2a4caa84bb57d58f61d3e5db253088e
timeobonnel21-commits <timeo.bonnel21@gmail.com>
2025-12-11 16:36:54 +0100

    content update

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,15 +9,13 @@ const data = [
     title: `GIMS-LA MANO PARISIENE`,
     src: `gims-parisiene.mp3`,
     category: `fun`,
-    cover:
-      "https://i.scdn.co/image/ab67616d0000b27393db7b33b63de6dc99a131ee",
+    cover: "https://i.scdn.co/image/ab67616d0000b27393db7b33b63de6dc99a131ee",
   },
   {
     title: `MON BÉBÉ`,
     src: `monbebe.mp3`,
     category: `RAP`,
-    cover:
-      "https://i.scdn.co/image/ab67616d0000b273deb7071c9722a7c10a26ed6b",
+    cover: "https://i.scdn.co/image/ab67616d0000b273deb7071c9722a7c10a26ed6b",
   },
   {
     title: `QUEEN - Another One Bites the Dust`,
@@ -25,17 +23,37 @@ const data = [
     category: `remix`,
     cover: "https://i.ytimg.com/vi/NVIbCvfkO3E/maxresdefault.jpg",
   },
-    {
+  {
     title: `Cyberpunk 2077`,
     src: `cyberpunk 2077.mp3`,
-    category: `thechno`,
-    cover: "https://image.api.playstation.com/vulcan/ap/rnd/202008/0416/6Bo40lnWU0BhgrOUm7Cb6by3.png",
+    category: `techno`,
+    cover:
+      "https://image.api.playstation.com/vulcan/ap/rnd/202008/0416/6Bo40lnWU0BhgrOUm7Cb6by3.png",
   },
-    {
+  {
     title: `R2-nuinart`,
     src: `R2-ruinart.mp3`,
     category: `RAP`,
-    cover: "https://cdn-images.dzcdn.net/images/cover/405f1c98886bd62846f7609df66ce1f2/0x1900-000000-80-0-0.jpg",
+    cover:
+      "https://cdn-images.dzcdn.net/images/cover/405f1c98886bd62846f7609df66ce1f2/0x1900-000000-80-0-0.jpg",
+  },
+  {
+    title: `attenta`,
+    src: `Attantat.mp3`,
+    category: `RAP fun`,
+    cover: "https://i.scdn.co/image/ab67616d0000b273e7920f048947a2cb6371557a",
+  },
+  {
+    title: `Copine`,
+    src: `Copine.mp3`,
+    category: `RAP`,
+    cover: "https://i.scdn.co/image/ab67616d0000b273d34da31b55f52c4c06a12dff",
+  },
+  {
+    title: `Loreen - Tattoo`,
+    src: `Loreen - Tattoo.mp3`,
+    category: `POP`,
+    cover: "https://i.scdn.co/image/ab67616d0000b2732b0ba87db609976eee193bd6",
   },
 ];
 console.table(data);
@@ -45,8 +63,9 @@ data.forEach((music) => {
             <div class="cardaudio linear-gradient">
               <div class="cover">
                 <img
-                  src=" ${music.cover} "
-                  alt="cover"
+                  src="${music.cover}"
+                  alt="${music.title}"
+                  loading="lazy"
                 />
               </div>
               <div class="content">
