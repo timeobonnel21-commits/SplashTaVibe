0dcd9ad1c5edc7b1e0ad1d9a5187732b7694afec
timeobonnel21-commits <timeo.bonnel21@gmail.com>
2025-12-11 11:14:50 +0100

    js: add playliste

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,69 +1,38 @@
-const version = "v.0.1.0";
+const author = `timeo`;
+console.log(`welcome voici l'été: "citation de ${author} "`);
 
-console.log(21.5);
-
-const year = 2025;
-const dob = 2011;
-const isAdult = true;
-const user = {
-  name: "lisa",
-  age: 20,
-};
-const family = ["Jéremy", "Mélanie", "Clara", "Lisa", "Timéo", "Joy"];
-console.log(typeof family);
-const age = year - dob;
-console.log(age);
-
-const userName = "timeo";
-console.log("bonjour " + userName + " bienvenue sur mon site");
-
-console.log(`bonsoir ${family[3]},je connais ton age: ${user.age}`);
-
-console.table(family);
-console.table(user);
-console.warn("Attention au hack");
-console.error("erreur 21");
-
-const fullFamily = [
-  { name: "jeremy", age: 45, isAdult: true },
-  { name: "timeo", age: 14, isAdult: false },
+const data = [
+  {
+    title: `GIMS-LA MANO PARISIENE`,
+    scr: `gims-parisiene.mp3`,
+    category: `fun`,
+    cover:
+      "https://img.static-af.com/transform/45cb9a13-b167-4842-8ea8-05d0cc7a4d04/",
+  },
+  {
+    title: `MON BÉBÉ`,
+    scr: `monbebe.mp3`,
+    category: `RAP`,
+    cover:
+      "https://static.vecteezy.com/ti/vecteur-libre/t2/1312577-mignon-heureux-baby-holding-baby-milk-bottle-vectoriel.jpg",
+  },
 ];
-console.table(fullFamily);
-console.log(fullFamily[1].name);
 
-function hello(name) {
-  console.log(`bonjour ${name}`);
-}
-hello(family[3]);
-hello(family[5]);
-
-family.forEach((user) => {
-  console.log(`salut ${user}`);
+data.forEach((music) => {
+  document.getElementById(`playlist`).innerHTML += `<li>
+            <div class="cardaudio linear-gradient">
+              <div class="cover">
+                <img
+                  src=" ${music.cover} "
+                  alt="cover"
+                />
+              </div>
+              <div class="content">
+                <h3>${music.title}</h3>
+                <p>categorie : ${music.category} </p>
+              </div>
+            </div>
+          </li>`;
 });
 
-console.log(document.getElementById("version"));
-document.getElementById("version").textContent = version;
-
-// premier etape declare les piste audio
-
-const audio = {
-  title: "string",
-  category: "string",
-  cover: "string",
-  src: "string",
-};
-
-const playlist = [
-  audio,
-  audio,
-  audio,
-  audio,
-  audio,
-  audio,
-  audio,
-  audio,
-  audio,
-  audio,
-  audio,
-];
-console.table(playlist);
+document.querySelector("")
