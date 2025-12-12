0261b53fc9a020ecce78dd933cda73ea957332a6
timeobonnel21-commits <timeo.bonnel21@gmail.com>
2025-12-12 12:50:46 +0100

    js: import export data architecture

---
+++ assets/js/data-audio.js
@@ -0,0 +1,58 @@
+export const data = [
+  {
+    title: `GIMS-LA MANO PARISIENE`,
+    src: `gims-parisiene.mp3`,
+    category: `fun`,
+    cover: "https://i.scdn.co/image/ab67616d0000b27393db7b33b63de6dc99a131ee",
+  },
+  {
+    title: `MON BÉBÉ`,
+    src: `monbebe.mp3`,
+    category: `RAP`,
+    cover: "https://i.scdn.co/image/ab67616d0000b273deb7071c9722a7c10a26ed6b",
+  },
+  {
+    title: `QUEEN - Another One Bites the Dust`,
+    src: `queen.mp3`,
+    category: `remix`,
+    cover: "https://i.ytimg.com/vi/NVIbCvfkO3E/maxresdefault.jpg",
+  },
+  {
+    title: `Cyberpunk 2077`,
+    src: `cyberpunk 2077.mp3`,
+    category: `techno`,
+    cover:
+      "https://image.api.playstation.com/vulcan/ap/rnd/202008/0416/6Bo40lnWU0BhgrOUm7Cb6by3.png",
+  },
+  {
+    title: `R2-nuinart`,
+    src: `R2-ruinart.mp3`,
+    category: `RAP`,
+    cover:
+      "https://cdn-images.dzcdn.net/images/cover/405f1c98886bd62846f7609df66ce1f2/0x1900-000000-80-0-0.jpg",
+  },
+  {
+    title: `attenta`,
+    src: `Attentat.mp3`,
+    category: `RAP fun`,
+    cover: "https://i.scdn.co/image/ab67616d0000b273e7920f048947a2cb6371557a",
+  },
+  {
+    title: `Copine`,
+    src: `Copine.mp3`,
+    category: `RAP`,
+    cover: "https://i.scdn.co/image/ab67616d0000b273d34da31b55f52c4c06a12dff",
+  },
+  {
+    title: `Loreen - Tattoo`,
+    src: `Loreen - Tattoo.mp3`,
+    category: `POP`,
+    cover: "https://i.scdn.co/image/ab67616d0000b2732b0ba87db609976eee193bd6",
+  },
+  {
+    title: `hardwell & maddix`,
+    src: `hardwellmaddix.mp3`,
+    category: `ACID`,
+    cover: "https://i.scdn.co/image/ab67616d0000b27337d8edae9f22963c480e8729",
+  },
+];
